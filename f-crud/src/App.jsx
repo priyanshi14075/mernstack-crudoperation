d51a548b0e4c59eea8b4215ee9560c33a140e6ed
priyanshi14075 <priyanshidholariya3@gmail.com>
2025-12-07 10:22:27 +0530

    initial commit

---
+++ f-crud/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './components/Users';
+import CreateUsers from './components/CreateUser';
+import UpdateUsers from './components/UpdateUser';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Users />}></Route>
+        <Route path='/create' element={<CreateUsers />}></Route>
+        <Route path='/update' element={<UpdateUsers />}></Route>
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./model/Users')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+app.get('/', (req, res) =>{
+    UserModel.find({})
+    .then(users => res .json(users))
+    .catch(err => res.json(err))
+})
+
+app.post("/createUser", (req , res) =>{
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => req.json(err))
+} )
+
+app.listen(3001, () =>{
+    console.log("server is Running")
+})
